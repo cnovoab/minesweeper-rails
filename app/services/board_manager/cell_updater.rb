cb7b6b0496ff8e02c1b041260b2e20dad53c49d4
Claudio Novoa <claudio.novoa.b@gmail.com>
2020-06-21 20:55:28 -0400

    chore: refactor cell updater service

--- app/services/board_manager/cell_updater.rb
+++ app/services/board_manager/cell_updater.rb
@@ -2,11 +2,11 @@ module BoardManager
   class CellUpdater < ApplicationService
     PARAM_KEYS = %w(game_id row col)
     CELL_KEYS = %w(flagged revealed mine value)
+
     def initialize(params)
       game_id, row, col = params.values_at(*PARAM_KEYS)
       @game = Game.find(game_id)
-      @row = row.to_i
-      @col = col.to_i
+      @row, @col = row.to_i, col.to_i
       @cell = @game.board[@row][@col]
       @cell_value = params.values_at(*CELL_KEYS)
     end
